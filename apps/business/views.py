8487288f71e2011152c8a667b5a3b5872fbb9e25
johnnyio <jokpo2565@gmail.com>
2022-08-08 04:08:32 +0100

    #17 Committing the changes made to bussiness and text files

--- apps/business/views.py
+++ apps/business/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from .models import Business
+from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render, redirect
 
+def generate_text_qr_code(request):
+    if request.user.is_anonymous:
+        return redirect("generate_code")
+    template_name = "texts/text_view.html"
+    if request.method == "POST":
+         name = request.POST.get("text_name")
+         bussiness_name = request.POST.get("bussiness_name")
+         email = request.POST.get('email')
+         phone_no = request.POST.get('phone_number')
+         location = request.POST.get("location")
+         bio = request.POST.get('bio')
+         logo = request.POST.get('logo')
 
-
-
-def generate_qr_code(request):
-    pass
+         business = Business.objects.create(
+             name=name, 
+             bussiness_name=bussiness_name, 
+             email= email, 
+             phone_no = phone_no, 
+             location = location,
+             bio = bio,
+         )
+         if business is not None:
+             business.save()
+             context = {"business": business}
+             messages.success(request, f"qr code generated")
+             return render(request, template_name, context)
+         return redirect("generate_code")
+    return render(request, template_name)
+ 
+ 
+def get_text_qr_code(request, id):
+    template_name = "texts/qr_info"
+    text_context = get_object_or_404(Text, id=id)
+    context = {"texts": text_context}
+    return render(request, template_name, context)
--- apps/texts/views.py
+++ apps/texts/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
-
-
-
-
-def generate_qr_code(request):
-    pass
-
-# def get_text_detail(request):
-#     template_name = ''
+from .models import Text
+from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render, redirect
+ 
+ 
+def generate_text_qr_code(request):
+    if request.user.is_anonymous:
+        return redirect("generate_code")
+    template_name = "texts/text_view.html"
+    if request.method == "POST":
+         name = request.POST.get("text_name")
+         description = request.POST.get("description")
+         text = Text.objects.create(
+             name=name, description=description, created_by=request.user
+         )
+         if text is not None:
+             text.save()
+             context = {"text": text}
+             messages.success(request, f"qr code generated")
+             return render(request, template_name, context)
+         return redirect("generate_code")
+    return render(request, template_name)
+ 
+ 
+def get_text_qr_code(request, id):
+    template_name = "texts/qr_info"
+    text_context = get_object_or_404(Text, id=id)
+    context = {"texts": text_context}
+    return render(request, template_name, context)
